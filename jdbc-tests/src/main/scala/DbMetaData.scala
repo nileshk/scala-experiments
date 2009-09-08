08719600b65ab0fa7326d3c67bbee70d11396082
Nilesh Kapadia <git@nileshk.com>
2009-09-07 22:49:33 -0400

    Get table column metadata

--- jdbc-tests/src/main/scala/DbMetaData.scala
+++ jdbc-tests/src/main/scala/DbMetaData.scala
@@ -8,10 +8,7 @@ class DbMetaData(connection: Connection) {
   private val tableTypes = Array("TABLE")
   
   def tableNames(schema: String): List[String] = {
-    val rs = metaData.getTables(null,
-                                schema,
-                                null,
-                                tableTypes)
+    val rs = metaData.getTables(null, schema, null, tableTypes)
     val tables =
       processResultSet(rs,
         resultSet => resultSet.getString("TABLE_NAME"))
@@ -20,20 +17,28 @@ class DbMetaData(connection: Connection) {
   }
   
   def tableData(schema: String, table: String): List[TableData] = {
-    val rs = metaData.getTables(null,
-                                schema,
-                                table,
-                                tableTypes)
+    val rs = metaData.getTables(null, schema, table, tableTypes)
     val tables =
       processResultSet(rs,
         resultSet => {
           new TableData(schema,
                         resultSet.getString("TABLE_NAME"),
                         resultSet.getString("TABLE_TYPE"),
-                        resultSet.getString("REMARKS"))})
+                        resultSet.getString("REMARKS"),
+                        columnData(schema, table))})
     rs.close()
     return tables
       
+  }
+
+  def columnData(schema: String, table: String): List[ColumnData] = {
+    val rs = metaData.getColumns(null, schema, table, null)
+    val columns =
+      processResultSet(rs,
+        resultSet => {
+          new ColumnData(resultSet.getString("COLUMN_NAME"))})
+    rs.close()
+    return columns
   }
 
   private def processResultSet[T](rs: ResultSet, mapFunc: ResultSet => T): List[T] = {
@@ -50,8 +55,15 @@ class TableData(
   schema: String,
   name: String,
   tableType: String,
-  remarks: String) {
+  remarks: String,
+  columns: List[ColumnData]) {
 
   override def toString() = 
-    schema + ":" + name + ":" + tableType + ":" + remarks
+    schema + ":" + name + ":" + tableType + ":" + remarks + "\n" +
+      columns.mkString("   (", ", ", ")")
+}
+
+class ColumnData(name: String) {
+  override def toString() =
+    name
 }
